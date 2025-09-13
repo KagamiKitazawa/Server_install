1d58297ac62b4193ff083b6840aa324238007426
Zhe Wang <kagami@kitazawa.org>
2025-09-14 06:23:50 +0800

    Update server-install.sh

--- server-install.sh
+++ server-install.sh
@@ -71,7 +71,7 @@ apt install ca-certificates curl -y
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bookworm stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian trixie stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update -y
 apt install docker-ce docker-ce-cli containerd.io -y
 # 安装 Docker Compose
